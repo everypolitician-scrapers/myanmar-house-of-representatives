41fd419d5ea129622b73cf537bbc6507093423e5
Tony Bowden <tony@tmtm.com>
2017-01-11 08:49:09 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -10,7 +11,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -21,17 +22,17 @@ end
 def scrape_list(url)
   noko = noko_for(url)
   noko.css('#representative-region').each do |rep|
-    source = URI.join("http://www.pyithuhluttaw.gov.mm/", rep.at_css('a[href*="q=representative/"]/@href').text).to_s
-    data = { 
-      id: File.basename(source).tr('%',''),
-      name: rep.css('.region-representative-title .field-content').text.tidy,
-      party: rep.css('.region-representative-party .field-content').text.tidy,
-      area: rep.css('.region-constituency').text.tidy,
-      image: rep.css('.region-representative-photo img/@src').text,
-      term: 1,
+    source = URI.join('http://www.pyithuhluttaw.gov.mm/', rep.at_css('a[href*="q=representative/"]/@href').text).to_s
+    data = {
+      id:     File.basename(source).tr('%', ''),
+      name:   rep.css('.region-representative-title .field-content').text.tidy,
+      party:  rep.css('.region-representative-party .field-content').text.tidy,
+      area:   rep.css('.region-constituency').text.tidy,
+      image:  rep.css('.region-representative-photo img/@src').text,
+      term:   1,
       source: source,
     }
-    ScraperWiki.save_sqlite([:id, :term], data)
+    ScraperWiki.save_sqlite(%i(id term), data)
   end
 end
 
